4b21cdcf73008d223cd9295fa9162acc7da73d5c
EdersonDav <silva.edersonqueiroz@gmail.com>
2020-08-26 12:47:37 -0300

    Finish

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,14 @@ interface Balance {
   total: number;
 }
 
+interface TransictionTDO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +22,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { outcome, income } = this.transactions.reduce(
+      (accumalator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumalator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumalator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return accumalator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    const balance = { outcome, income, total };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransictionTDO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransictionServiceTDO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransictionServiceTDO): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    if (type === 'outcome') {
+      if (value > balance.total) {
+        throw Error('Your balance not sufficient');
+      }
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+    return transaction;
   }
 }
 
